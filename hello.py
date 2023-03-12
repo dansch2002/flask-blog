6b1dd03c4d4baa44f0abd67571c66c084ab92c18
Dan Schumacher <dan.schumacher@gmail.com>
2023-03-11 21:27:05 -0600

    Changes 05

--- hello.py
+++ hello.py
@@ -1,7 +1,51 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "SecretKey123"
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+# BooleanField
+	# DateField
+	# DateTimeField
+	# DecimalField
+	# FileField
+	# HiddenField
+	# MultipleField
+	# FieldList
+	# FloatField
+	# FormField
+	# IntegerField
+	# PasswordField
+	# RadioField
+	# SelectField
+	# SelectMultipleField
+	# SubmitField
+	# StringField
+	# TextAreaField
+
+	## Validators
+	# DataRequired
+	# Email
+	# EqualTo
+	# InputRequired
+	# IPAddress
+	# Length
+	# MacAddress
+	# NumberRange
+	# Optional
+	# Regexp
+	# URL
+	# UUID
+	# AnyOf
+	# NoneOf
 
 
 # Create a route decorator
@@ -29,12 +73,10 @@ def index():
                            stuff1=stuff1,
                            favorite_pizza=favorite_pizza)
 
-
 # localhost:5000/user/John
 @app.route('/user/<name>')
 def user(name):
     return render_template("user.html", name=name)
-
 
 # Create Custom Error Pages
 
@@ -43,8 +85,21 @@ def user(name):
 def page_not_found(e):
     return render_template("404.html"), 404
 
-
 # Internal Server Error
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template("name.html", name = name, form = form)
